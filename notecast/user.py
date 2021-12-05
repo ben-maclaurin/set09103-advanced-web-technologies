baef0855fad067bb82035759bc5ebe90c0d3bdb7
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-12-05 23:55:17 +0000

    feat(user): add assign_user, logout and login_required

--- notecast/user.py
+++ notecast/user.py
@@ -11,6 +11,17 @@ from notecast.database import get_database
 
 blueprint = Blueprint("user", __name__, url_prefix="/user")
 
+@blueprint.before_app_request
+def assign_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_database().execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+
 @blueprint.route("/signup", methods=("GET", "POST"))
 def sign_up():
     if request.method == "POST":
@@ -38,3 +49,18 @@ def sign_up():
         flash(error)
 
     return render_template("user/signup.html")
+
+@blueprint.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("user.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
