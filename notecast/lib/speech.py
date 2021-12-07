cb9120f8202c4d1616f67c2bf1482b9afc2c0033
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-12-07 12:52:35 +0000

    refactor(lib): slight refactor of tts functions

--- notecast/lib/speech.py
+++ notecast/lib/speech.py
@@ -2,11 +2,12 @@
 import requests
 import os
 
+def retrieve_token():
+    headers = {'Ocp-Apim-Subscription-Key': os.environ.get("MICROSOFT_KEY")}
+    response = requests.post("https://" + os.environ.get("MICROSOFT_REGION") + ".api.cognitive.microsoft.com/sts/v1.0/issuetoken", headers = headers)
+
+    return response.text
+
+
 def synthesise_speech(script):
-    body = {'Ocp-Apim-Subscription-Key': os.environ.get("MICROSOFT_KEY")}
-    response = requests.post("https://" + os.environ.get("MICROSOFT_REGION") + ".api.cognitive.microsoft.com/sts/v1.0/issuetoken", headers = body)
-
-    print(os.environ.get("MICROSOFT_KEY"))
-    print(os.environ.get("MICROSOFT_REGION"))
-
-    print(response.text)
+    token = retrieve_token()
